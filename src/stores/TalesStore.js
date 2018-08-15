f00958e9389302bcaf784112b5b4639864d643c2
Колпаков Александр Сергеевич <Kolpakov.A.Se@sberbank.ru>
2018-08-15 16:06:09 +0300

    feat(phrases): start adding phrases.

---
+++ src/stores/TalesStore.js
@@ -0,0 +1,13 @@
+import { observable, action, decorate } from 'mobx';
+
+class TalesStore {
+    constructor() {
+        
+    }
+}
+
+decorate(TalesStore, {
+
+});
+
+export default new TalesStore();
